08fb80a01beea4ecd9e949b3544a8fd46159aedf
rbv <ruben@atipics.com>
2012-03-17 13:31:15 +0100

    added Sponsor entity

---
+++ NSEvents/NSEvents/Sponsor.h
@@ -0,0 +1,13 @@
+//
+//  Sponsor.h
+//  NSEvents
+//
+//  Created by Rubén Bernal Verneda on 17/03/12.
+//  Copyright (c) 2012 Atípic software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sponsor : NSObject
+
+@end
---
+++ NSEvents/NSEventsTests/SponsorTests.m
@@ -0,0 +1,13 @@
+//
+//  SponsorTests.m
+//  NSEvents
+//
+//  Created by Rubén Bernal Verneda on 17/03/12.
+//  Copyright (c) 2012 Atípic software. All rights reserved.
+//
+
+#import "SponsorTests.h"
+
+@implementation SponsorTests
+
+@end
