cff0e29c041e0177101ecf2558f477a37b4f2dfb
ileider <ileider@gmail.com>
2012-03-17 18:19:05 +0100

    changed news cell

---
+++ NSEvents/NSEvents/UI/NewsCell.h
@@ -0,0 +1,13 @@
+//
+//  NewsCell.h
+//  NSEvents
+//
+//  Created by Ivan Leider on 17/03/12.
+//  Copyright (c) 2012 Atípic software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsCell : UITableViewCell
+@property (nonatomic, strong) IBOutlet UILabel *text;
+@end
--- NSEvents/NSEvents/UI/NewsTableViewController.m
+++ NSEvents/NSEvents/UI/NewsTableViewController.m
@@ -8,7 +8,7 @@
 
 #import "NewsTableViewController.h"
 #import <Twitter/Twitter.h>
-
+#import "NewsCell.h"
 @interface NewsTableViewController ()
 @property (nonatomic, strong) NSArray *twitterResponse;
 @property (nonatomic, strong) UIActivityIndicatorView *activity;
@@ -107,10 +107,10 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
   static NSString *CellIdentifier = @"TwitterCell";
-  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  NewsCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
   
   // Configure the cell...
-  cell.textLabel.text = [[self.twitterResponse objectAtIndex:indexPath.row] objectForKey:@"text"];
+  cell.text.text = [[self.twitterResponse objectAtIndex:indexPath.row] objectForKey:@"text"];
   return cell;
 }
 
