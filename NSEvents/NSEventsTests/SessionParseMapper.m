6ffe22b223eb250486b92e05b0a1f7b8adcd1c10
rbv <ruben@atipics.com>
2012-03-17 17:58:29 +0100

    SessionParseMapper hashtag twitter

---
+++ NSEvents/NSEventsTests/SessionParseMapper.m
@@ -0,0 +1,15 @@
+#import "SessionParseMapper.h"
+
+#import <UIKit/UIKit.h>
+//#import "application_headers" as required
+
+@implementation SessionParseMapper
+
+// All code under test is in the iOS Application
+- (void)testAppDelegate
+{
+    id yourApplicationDelegate = [[UIApplication sharedApplication] delegate];
+    STAssertNotNil(yourApplicationDelegate, @"UIApplication failed to find the AppDelegate");
+}
+
+@end
