0ee73fa25144f805436d3e08d4ae83dd9d13d0b9
rbv <ruben@atipics.com>
2012-03-17 18:20:55 +0100

    Fix compilation errors

---
+++ NSEvents/NSEvents/SessionParseMapper.m
@@ -0,0 +1,13 @@
+//
+//  SessionParseMapper.m
+//  NSEvents
+//
+//  Created by Rubén Bernal Verneda on 17/03/12.
+//  Copyright (c) 2012 Atípic software. All rights reserved.
+//
+
+#import "SessionParseMapper.h"
+
+@implementation SessionParseMapper
+
+@end
