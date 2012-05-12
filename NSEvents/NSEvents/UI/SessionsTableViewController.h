eca33aa470b16949ba16a9b5b2224f0e76258b7c
ileider <ileider@gmail.com>
2012-05-12 12:14:09 +0200

    added sessions and speakers view controllers, testing data retrieval from parse

---
+++ NSEvents/NSEvents/UI/SessionsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  SessionsTableViewController.h
+//  NSEvents
+//
+//  Created by Ivan Leider on 12/05/12.
+//  Copyright (c) 2012 Atípic software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Event;
+
+@interface SessionsTableViewController : UITableViewController
+
+@property (nonatomic, strong) Event *event;
+
+@end
