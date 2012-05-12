6ad8b5593cb1cbc0e8f322d610a480f7fded4b2b
Joan F. Guerrero <jfguerreroestrada@gmail.com>
2012-05-12 13:37:04 +0200

    TimeAndPlace con sus labels enlazadas

--- NSEvents/NSEvents/UI/TimeAndPlaceViewController.m
+++ NSEvents/NSEvents/UI/TimeAndPlaceViewController.m
@@ -31,21 +31,21 @@
     [super viewDidLoad];
     NSLog(@"Evento %@",event.title);
     
-    event.location=[[Location alloc]init];
+    self.event.location=[[Location alloc]init];
     
     
-    event.location.title = event.title;
-    event.location.address = @"Direccion";
-    event.location.city = @"Ciudad";
-    event.location.country = @"Country";
-    event.location.latitud = [NSNumber numberWithFloat:41.408943];
-    event.location.longitud = [NSNumber numberWithFloat:2.126601];
+    self.event.location.title = event.title;
+    self.event.location.address = @"Direccion";
+    self.event.location.city = @"Ciudad";
+    self.event.location.country = @"Country";
+    self.event.location.latitud = [NSNumber numberWithFloat:41.408943];
+    self.event.location.longitud = [NSNumber numberWithFloat:2.126601];
     
-    locationPoint= [[LocationPoint alloc]initWithTitle:event.location.title address:event.location.address city:event.location.city latitud:[event.location.latitud floatValue] longitud:[event.location.longitud floatValue]];
+    self.locationPoint= [[LocationPoint alloc]initWithTitle:event.location.title address:event.location.address city:event.location.city latitud:[event.location.latitud floatValue] longitud:[event.location.longitud floatValue]];
     
-    [mapView addAnnotation:locationPoint];
+    [self.mapView addAnnotation:locationPoint];
    
-    MKCoordinateSpan span=MKCoordinateSpanMake(9, 9);
+    MKCoordinateSpan span=MKCoordinateSpanMake(0.1, 0.1);
 
     MKCoordinateRegion region;
 
@@ -57,11 +57,13 @@
     
     NSDateFormatter *start=[[NSDateFormatter alloc]init];
     NSDateFormatter *end =[[NSDateFormatter alloc]init];
+    start.dateStyle=NSDateFormatterFullStyle;
+    end.dateStyle=NSDateFormatterFullStyle;
     
-    lblTitle.text=event.title;
-    lblStartDate.text=[start stringFromDate:event.startDate];
-    lblEndDate.text=[end stringFromDate:event.startDate];
-    lblHashtag.text= event.hashtag;
+    self.lblTitle.text=event.title;
+    self.lblStartDate.text=[start stringFromDate:event.startDate];
+    self.lblEndDate.text=[end stringFromDate:event.startDate];
+    self.lblHashtag.text= event.hashtag;
     
 	// Do any additional setup after loading the view.
 }
