46abf138dfad1d988fb27da641d061fa702c9d68
Leandro Melo Aranda <lmeloa@me.com>
2012-05-12 13:51:39 +0200

    CopyRight NSCoders

--- NSEvents/NSEvents/main.m
+++ NSEvents/NSEvents/main.m
@@ -2,8 +2,6 @@
 //  main.m
 //  NSEvents
 //
-//  Created by Rubén Bernal Verneda on 17/03/12.
-//  Copyright (c) 2012 Atípic software. All rights reserved.
 //  Created by "Asociación NSCoders ESP" on 17/03/12.
 //  Copyright (c) 2012 NSCodersESP. All rights reserved.
 //
